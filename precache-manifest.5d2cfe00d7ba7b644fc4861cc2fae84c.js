2caed4d9e7ab85e2c2c9fe3d1318c42276b984dd
Nesia Shafira <nesia.shafira@gmail.com>
2019-04-08 16:49:30 +0700

    first commit

---
+++ precache-manifest.5d2cfe00d7ba7b644fc4861cc2fae84c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "aafac543edde96feea58bafed7c99cef",
+    "url": "/static/media/smp19.aafac543.jpg"
+  },
+  {
+    "revision": "dccf92e70d76fc70d384",
+    "url": "/static/css/main.8d683528.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6f74c6a74f74184d98b4",
+    "url": "/static/js/2.9c9ced7e.chunk.js"
+  },
+  {
+    "revision": "acbad731db9e85214a904ccb99111884",
+    "url": "/static/media/Arielfunny.acbad731.png"
+  },
+  {
+    "revision": "e498b9c86dc7e474dfba3345e0e00c01",
+    "url": "/static/media/muhammadiyah.e498b9c8.jpg"
+  },
+  {
+    "revision": "dccf92e70d76fc70d384",
+    "url": "/static/js/main.3f5ef87f.chunk.js"
+  },
+  {
+    "revision": "cb90dc872a059c5319d5ed248c61a684",
+    "url": "/static/media/sman70.cb90dc87.jpg"
+  },
+  {
+    "revision": "e6f2dfd075230b5a8020fd1803ce4146",
+    "url": "/static/media/binus.e6f2dfd0.jpg"
+  },
+  {
+    "revision": "48a4ff533378917f515f8e828f2e61fb",
+    "url": "/static/media/css.48a4ff53.png"
+  },
+  {
+    "revision": "e17c99cbe7634fa3b9fe509da281c9b5",
+    "url": "/static/media/js.e17c99cb.png"
+  },
+  {
+    "revision": "3fc15b2c03d853d87da4a463c7a9d7bf",
+    "url": "/static/media/html.3fc15b2c.png"
+  },
+  {
+    "revision": "485c128b527465d8fc6d93b067847909",
+    "url": "/index.html"
+  }
+];
